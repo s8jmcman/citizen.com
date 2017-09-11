719c2f68482130bfec994ebb737ccd4bfd9c7716
s8jmcman <s8jmcman@gmail.com>
2017-09-10 21:14:23 -0700

    Add existing file

---
+++ citizen/Citizen/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Citizen.Startup))]
+namespace Citizen
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
